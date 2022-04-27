6edfe5cc170c21a74b7666942433e8710cc7b938
yaobinze <yaobinze@foxmail.com>
2022-04-28 00:05:15 +0800

    [*]fixed in api and ico

---
+++ src/api/request.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+export function request (config) {
+  // 1 创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://152.136.185.210:7878/api/m5',
+    timeout: 5000
+  })
+
+  // 2 axios的拦截器 interceptors
+  // 2.1请求拦截的过程
+  instance.interceptors.request.use(config => {
+    //  console.log(config);
+    return config
+  }, err => {
+    //  console.log(err);
+  })
+
+  //  2.2响应拦截的过程
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, err => {
+    console.log(err);
+  })
+
+  // 3 发送真正的网络请求
+  // 本身返回的就是Promise  AxiosPromise
+  return instance(config)
+}
